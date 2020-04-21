bf873f15153ad081c436e8dc92228f458b0bbc5a
brian-noh <brian-noh@users.noreply.github.com>
2020-04-21 04:30:38 -0400

    Finished ModuleList

--- src/app/module-list/module-list.component.ts
+++ src/app/module-list/module-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-module-list',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModuleListComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private route: ActivatedRoute) { }
+  courseId = ''
+  moduleId = ''
+  modules = [
+    {_id: '123', title: 'module 1'},
+    {_id: '234', title: 'module 2'},
+    {_id: '345', title: 'module 3'}
+  ];
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.courseId = params.cid;
+      this.moduleId = params.mid;
+    })
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/001272260/courses/${this.courseId}/modules`)
+      .then(response => response.json())
+      .then(modules => this.modules = modules);
   }
 
 }
--- src/app/services/CourseServiceClient.ts
+++ src/app/services/CourseServiceClient.ts
@@ -4,6 +4,6 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class CourseServiceClient {
   findAllCourses = () =>
-    fetch('https://wbdv-generic-server.herokuapp.com/api/jannunzi/courses')
+    fetch('https://wbdv-generic-server.herokuapp.com/api/001272260/courses')
       .then(response => response.json())
 }
